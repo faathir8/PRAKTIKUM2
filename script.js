ba297a9388bc78343783a0c62663d5b8a01a7ce9
faathir8 <faathirmhd81@gmail.com>
2024-11-10 23:02:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle mode tampilan
+const toggleButton = document.getElementById("toggleMode");
+toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("messy");
+});
